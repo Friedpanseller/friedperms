38428ff1ba98df2ca4fa49f4d19c12707a8659ae
Friedpanseller <friedpanseller@gmail.com>
2018-06-07 12:50:13 +0000

    A

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 #domain=`echo "$1" | sed 's/\./\\./g'`
 for i in `ls splits`; do #i is the wordlist name
 	#cat $PWD/splits/$i | sed 's/$/\.'$domain'/g' | ../zdns A | grep "NOERROR" | jq '.["name"]' | sed 's/\"//g' > results/$i.txt&
-	python2 ../dnscan/dnscan.py -d $1 -w splits/$i -t 64 -o results/$i.txt&
+	python2 ../dnscan/dnscan.py -d $1 -w splits/$i -t 64 > results/$i.txt&
 done
 
